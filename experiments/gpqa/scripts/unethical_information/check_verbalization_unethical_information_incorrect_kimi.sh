5d68873a3473a49ca4320494ec0327e8f3ee3569
Will Gantt <wgantt.iv@gmail.com>
2026-03-21 08:40:57 -0400

    update check verbalization scripts

--- experiments/gpqa/scripts/unethical_information/check_verbalization_unethical_information_incorrect_kimi.sh
+++ experiments/gpqa/scripts/unethical_information/check_verbalization_unethical_information_incorrect_kimi.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-CLAUDE_API_KEY=$ANTHROPIC_API_KEY
+OPENAI_API_KEY=$OPENAI_API_KEY
 
-CLAUDE_TEMPERATURE=0.0
-CLAUDE_MAX_TOKENS=2048
-CLAUDE_MAX_THINKING_TOKENS=0 # no thinking tokens for this task
-
+OPENAI_TEMPERATURE=0.0
+OPENAI_MAX_TOKENS=8192
+OPENAI_MAX_THINKING_TOKENS=4096 # not actually consumed
 
 ### Claude-4.5-Haiku: Grader Hacking Eval
+
 python experiments/prompt.py \
     --input-jsonl experiments/gpqa/prompts/check_verbalization/gpqa_diamond_verbalize_unethical_information_incorrect_kimi_k2_thinking_changed_check_verbalization.jsonl \
     --output-jsonl experiments/gpqa/results/check_verbalization/gpqa_diamond_verbalize_unethical_information_incorrect_kimi_k2_thinking_changed_check_verbalization.jsonl \
-    --model-type claude-haiku-4-5-20251001 \
-    --api-key $ANTHROPIC_API_KEY \
-    --temperature $CLAUDE_TEMPERATURE \
-    --max-tokens $CLAUDE_MAX_TOKENS \
-    --max-thinking-tokens $CLAUDE_MAX_THINKING_TOKENS \
+    --model-type gpt-5.4-2026-03-05 \
+    --api-key $OPENAI_API_KEY \
+    --temperature $OPENAI_TEMPERATURE \
+    --max-tokens $OPENAI_MAX_TOKENS \
+    --max-thinking-tokens $OPENAI_MAX_THINKING_TOKENS \
+    --reasoning-effort medium \
     --examples-per-write 200 \
     --max-concurrent-requests 20
 
 python experiments/evaluate_faithfulness.py \
     gpqa \
     experiments/gpqa/results/check_verbalization/gpqa_diamond_verbalize_unethical_information_incorrect_kimi_k2_thinking_changed_check_verbalization.jsonl \
-    experiments/gpqa/results/check_verbalization/gpqa_diamond_verbalize_unethical_information_incorrect_kimi_k2_thinking_changed_check_verbalization_results.json
+    experiments/gpqa/results/check_verbalization/gpqa_diamond_verbalize_unethical_information_incorrect_kimi_k2_thinking_changed_check_verbalization_results.json \
+    --include_per_example_results
