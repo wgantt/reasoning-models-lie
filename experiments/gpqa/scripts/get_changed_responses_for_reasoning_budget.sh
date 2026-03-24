e2f4eef9c70901039bd5127bf7134567f9ab18e0
Will Gantt <wgantt.iv@gmail.com>
2026-03-24 08:41:37 -0400

    check verbalization results by reasoning budget

---
+++ experiments/gpqa/scripts/get_changed_responses_for_reasoning_budget.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+PROJECT_ROOT=/home/hltcoe/wgantt/reasoning-models-lie
+ALL_RESPONSES=$PROJECT_ROOT/experiments/gpqa/results/all_responses
+CHANGED_RESPONSES=$PROJECT_ROOT/experiments/gpqa/results/changed_responses
+GET_CHANGED_RESPONSES=$PROJECT_ROOT/experiments/get_change_to_hint_examples.py
+
+CLAUDE_BASELINE=$ALL_RESPONSES/gpqa_diamond_verbalize_baseline_claude_4.5_haiku.jsonl
+
+BASELINES=("gpqa_diamond_verbalize_baseline_claude_4.5_haiku_2000.jsonl" "gpqa_diamond_verbalize_baseline_claude_4.5_haiku_4000.jsonl" "gpqa_diamond_verbalize_baseline_claude_4.5_haiku_8000.jsonl" "gpqa_diamond_verbalize_baseline_claude_4.5_haiku_16000.jsonl")
+
+HINTED_RESPONSES=("gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_2000.jsonl" "gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_4000.jsonl" "gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_8000.jsonl" "gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_16000.jsonl")
+
+# Claude 4.5 Haiku
+for i in "${!BASELINES[@]}"; do
+    BASELINE=$ALL_RESPONSES/${BASELINES[$i]}
+    HINTED_RESPONSE=$ALL_RESPONSES/${HINTED_RESPONSES[$i]}
+    # get file name
+    filename=$(basename -- "$HINTED_RESPONSE")
+    # remove extension
+    filename="${filename%.*}"
+    # ignore baseline
+    if [[ "$filename" == *"verbalize_baseline_claude_4.5_haiku"* ]]; then
+        continue
+    fi
+    python $GET_CHANGED_RESPONSES $BASELINE $HINTED_RESPONSE $CHANGED_RESPONSES/${filename}_changed.jsonl $CHANGED_RESPONSES/${filename}_stats.json
+done
---
+++ experiments/gpqa/scripts/grader_hacking/check_verbalization_grader_hacking_correct_claude_16000.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+OPENAI_API_KEY=$OPENAI_API_KEY
+
+OPENAI_TEMPERATURE=0.0
+OPENAI_MAX_TOKENS=8192
+OPENAI_MAX_THINKING_TOKENS=4096 # not actually consumed
+
+python experiments/prompt.py \
+    --input-jsonl experiments/gpqa/prompts/check_verbalization/gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_16000_changed_check_verbalization.jsonl \
+    --output-jsonl experiments/gpqa/results/check_verbalization/gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_16000_changed_check_verbalization.jsonl \
+    --model-type gpt-5.4-2026-03-05 \
+    --api-key $OPENAI_API_KEY \
+    --temperature $OPENAI_TEMPERATURE \
+    --max-tokens $OPENAI_MAX_TOKENS \
+    --max-thinking-tokens $OPENAI_MAX_THINKING_TOKENS \
+    --reasoning-effort medium \
+    --examples-per-write 200 \
+    --max-concurrent-requests 10
+
+python experiments/evaluate_faithfulness.py \
+    gpqa \
+    experiments/gpqa/results/check_verbalization/gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_16000_changed_check_verbalization.jsonl \
+    experiments/gpqa/results/check_verbalization/gpqa_diamond_verbalize_grader_hacking_correct_claude_4.5_haiku_16000_changed_check_verbalization_results.json \
+    --include_per_example_results
